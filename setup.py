d5a078afa6ab55efc32082545146758016bf4833
shenghuang <1367467330@qq.com>
2021-07-05 15:17:00 +0800

    update setup markdown path

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 
 # from setuptools import setup
 import setuptools
+filepath = 'README.md'
 setuptools.setup(
     name='RainbowPrint',
-    version='2.0.1',
+    version='2.0.2',
     author='hs',
     author_email='huangshengtx@163.com',
     description=u'在终端中输出彩色文本',
+    long_description=open(filepath, encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     install_requires=[],
 )
